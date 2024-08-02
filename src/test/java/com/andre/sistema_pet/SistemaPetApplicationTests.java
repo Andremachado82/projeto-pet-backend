cb3b44f3d03438a957d30cf8c5454f9a509c531a
André Machado <andrels.machado82@gmail.com>
2024-08-02 16:44:48 -0300

    Criando o projeto backend

---
+++ src/test/java/com/andre/sistema_pet/SistemaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.sistema_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
